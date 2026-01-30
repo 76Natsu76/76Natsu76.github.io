2ef6b80b1609a462d1f0f5ffa68e010e5e465479
76Natsu76 <80848505+76Natsu76@users.noreply.github.com>
2026-01-29 23:53:33 -0500

    Update and rename race-family-index.json to race-family-index.js

---
+++ race-family-index.js
@@ -0,0 +1,63 @@
+export const RACE_FAMILY_INDEX = {
+  humanoid: "humanoid",
+  elf: "humanoid",
+  dwarf: "humanoid",
+  orc: "humanoid",
+  goblinoid: "humanoid",
+  beastkin: "humanoid",
+
+  beast: "beast",
+  chimera: "beast",
+
+  plantfolk: "plantfolk",
+  slimeborn: "slimeborn",
+  insectoids: "insectoid",
+  arachnids: "arachnid",
+
+  undead: "undead",
+  spiritborn: "spirit",
+  spirit: "spirit",
+  fae: "fae",
+  construct: "construct",
+  machina: "construct",
+  techborn: "construct",
+
+  elementalborn: "elemental",
+  elemental: "elemental",
+
+  demonborn: "demon",
+  fiend: "demon",
+  demon: "demon",
+
+  dragonkin: "dragon",
+  draconian: "dragon",
+  dragon: "dragon",
+  wyrm: "dragon",
+
+  giantkin: "giantkin",
+  titanborn: "titan",
+  titan: "titan",
+  colossus: "titan",
+
+  eldritch: "eldritch",
+  aberration: "aberration",
+  astralborn: "astral",
+  chaosborn: "chaosborn",
+  mythic_beast: "mythic_beast",
+  mythic_undead: "mythic_undead",
+
+  primordial: "primordial",
+  voidborn: "void",
+  void: "void",
+  outer_god: "outer_god",
+
+  anomaly: "anomaly",
+  parasite: "parasite",
+  amorphous: "amorphous",
+  hivemind: "hivemind",
+  metaphysical_phenomenon: "metaphysical_phenomenon",
+
+  divinity: "divinity",
+  paragon: "paragon",
+  multiversal_paragon: "multiversal_paragon"
+};
