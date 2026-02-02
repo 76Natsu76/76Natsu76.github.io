758bbff6fc4e85c419a019a7f41984259899d314
76Natsu76 <80848505+76Natsu76@users.noreply.github.com>
2026-02-02 07:34:35 -0500

    Update region-unlock.js

--- region-unlock.js
+++ region-unlock.js
@@ -2,12 +2,12 @@ export const REGION_UNLOCKS = {
   "unlocks": {
     "forest": true,
     "plains": true,
-    "cavern": false,
-    "ruins": false,
-    "swamp": false,
-    "desert": false,
-    "tundra": false,
-    "mountains": false,
+    "cavern": true,
+    "ruins": true,
+    "swamp": true,
+    "desert": true,
+    "tundra": true,
+    "mountains": true,
     "void_realm": false,
     "astral_plane": false,
     "abyss": false
