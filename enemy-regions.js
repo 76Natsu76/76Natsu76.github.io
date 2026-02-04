35350dd1d9361ce9e42b2087fa8ba873d72dbcc2
76Natsu76 <80848505+76Natsu76@users.noreply.github.com>
2026-02-04 18:15:28 -0500

    Update enemy-regions.js

--- enemy-regions.js
+++ enemy-regions.js
@@ -1,193 +1,22 @@
-export const ENEMY_REGIONS = {
-  forest: [
-    "goblin", "tiny_slime", "slime", "giant_rat", "stray_hound",
-    "sticky_slime", "bandit", "feral_tracker", "cave_miner",
-    "cult_novice", "wolf", "corrosive_slime", "tunnel_delver",
-    "sigil_binder", "hobgoblin", "kobold", "alpha_mastiff",
-    "skeleton", "royal_slime", "orebreaker", "spirit_channeler",
-    "archer_rogue", "howling_warg", "deepstone_foreman",
-    "grand_occultist", "orc", "bandit_captain", "dire_wolf",
-    "skeleton_archer", "zombie", "ghoul", "giant_spider",
-    "hornet_swarm"
-  ],
+// ENEMY_REGIONS.js
+// Auto‑generated inverse mapping from REGION_ENEMIES.js
 
-  plains: [
-    "goblin", "bandit", "hobgoblin", "kobold", "alpha_mastiff",
-    "orc", "bandit_captain", "dire_wolf", "archer_rogue",
-    "ranger_scout"
-  ],
+import { REGION_ENEMIES } from "./region-enemies.js";
 
-  swamp: [
-    "tiny_slime", "slime", "sticky_slime", "corrosive_slime",
-    "royal_slime", "zombie", "ghoul", "giant_spider",
-    "hornet_swarm"
-  ],
+export const ENEMY_REGIONS = (() => {
+  const map = {};
 
-  cavern: [
-    "slime", "giant_rat", "sticky_slime", "cave_miner",
-    "tunnel_delver", "orebreaker", "deepstone_foreman",
-    "barbarian"
-  ],
+  for (const [region, enemies] of Object.entries(REGION_ENEMIES)) {
+    for (const enemy of enemies) {
+      if (!map[enemy]) map[enemy] = [];
+      map[enemy].push(region);
+    }
+  }
 
-  ruins: [
-    "cult_novice", "sigil_binder", "skeleton", "spirit_channeler",
-    "grand_occultist", "skeleton_archer", "zombie", "ghoul"
-  ],
+  // Sort region lists for consistency
+  for (const enemy of Object.keys(map)) {
+    map[enemy] = Array.from(new Set(map[enemy])).sort();
+  }
 
-  trainers_city: [
-    "cult_novice", "sigil_binder", "spirit_channeler",
-    "archer_rogue", "ranger_scout", "monk_disciple",
-    "witch_apprentice", "necromancer_adept", "paladin_initiate",
-    "assassin_shade", "seraphic_acolyte", "chrono_fractured_adept",
-    "aether_crowned_hexbinder", "spell_eater_homunculus",
-    "halo_crowned_squire", "sword_novice_duelist",
-    "arcane_bloom_student", "shadow_step_initiate",
-    "null_eater_sovereignling", "spell_riven_archonling"
-  ],
-
-  verdant_wildwood: [
-    "shaman_of_thorns", "druid_of_storms", "satyr_trickster",
-    "tiger_warrior", "nature_guardian"
-  ],
-
-  outcast_island: [
-    "zombie", "ghoul", "warlock_cultist", "assassin_shade",
-    "terror_knight", "necromancer_adept", "abyss_gnawed_shadehound",
-    "nullborn_ravager", "grimhollow_reaperling",
-    "night_torn_specterblade", "soul_husk_devourer",
-    "abyssal_bonecarver", "toxic_mire_stalker",
-    "banished_hex_caller"
-  ],
-
-  desert: [
-    "barbarian", "giant_boar", "serpent_guardian", "hellhound",
-    "hydra", "fire_elemental", "imp_warlock"
-  ],
-
-  mountains: [
-    "minotaur", "giant_boar", "earth_elemental", "wind_elemental",
-    "treant", "lightning_elemental", "metal_golem"
-  ],
-
-  tundra: [
-    "wraith", "wind_elemental", "ice_elemental"
-  ],
-
-  spirit_kingdom: [
-    "banshee", "fire_elemental", "water_elemental",
-    "earth_elemental", "wind_elemental", "lightning_elemental",
-    "ice_elemental", "poison_elemental", "arcane_sentinel",
-    "storm_sovereign", "runespun_aetherling",
-    "mana_starved_riftspawn", "ethereal_tidecaller",
-    "frost_veil_apparition", "storm_crowned_tempestling"
-  ],
-
-  arcstone_enclave: [
-    "sigil_binder", "spirit_channeler", "witch_apprentice",
-    "arcane_sentinel"
-  ],
-
-  abyssal_scar: [
-    "lesser_demon", "void_reaver", "abyss_watcher",
-    "infernal_juggernaut"
-  ],
-
-  void: [
-    "void_reaver", "funny_sthreeven_man", "void_leviathan"
-  ],
-
-  molten_underdeep: [
-    "chaos_wyrm", "molten_behemoth"
-  ],
-
-  celestial: [
-    "paladin_initiate"
-  ],
-
-  celestial_expanse: [
-    "celestial_guardian", "celestial_herald", "celestial_archon"
-  ],
-
-  arcane_rift: [
-    "arcane_chimera", "arcane_phoenix", "astral_chimera",
-    "primordial_wyrm"
-  ],
-
-  emberfang_ridge: [
-    "thunder_colossus"
-  ],
-
-  stormforge_sanctum: [
-    "thunder_colossus", "shadow_colossus", "primordial_wyrm",
-    "storm_titan"
-  ],
-
-  titanfall: [
-    "thunder_colossus", "glacier_titan", "nature_colossus",
-    "iron_colossus", "titan_of_ruin", "storm_titan",
-    "world_titan", "primeval_hornbeast", "ancient_bark_titan",
-    "sky_piercer_colossus", "obsidian_world_breaker",
-    "tide_forged_leviathan"
-  ],
-
-  void_frontier: [
-    "death_knight", "iron_colossus", "titan_of_ruin",
-    "infernal_archlord", "shadow_colossus", "void_monarch"
-  ],
-
-  shadow_labyrinth: [
-    "abyss_watcher", "death_knight"
-  ],
-
-  eternal_citadel: [
-    "divine_sentinel", "celestial_archon"
-  ],
-
-  radiant_ascension_spire: [
-    "angel", "radiant_dawnstrider", "sun_blessed_herald",
-    "seraphic_acolyte"
-  ],
-
-  seraphic_crucible: [
-    "celestial_spearwarden", "luminous_ascendant",
-    "heaven_bound_exemplar", "radiant_choir_seraphling"
-  ],
-
-  elderwood_heart: [
-    "verdant_fangprowler", "thorn_crowned_druidling",
-    "grove_warden_shaman", "grove_heart_oracle"
-  ],
-
-  primeval_overgrowth: [
-    "elder_thorn_reaver", "bloom_crowned_archdruid",
-    "primeval_rootshaper", "sylvan_apex_behemoth"
-  ],
-
-  celestial_horizon: [
-    "divine_forged_paragon", "halo_crested_archsentinel",
-    "seraphic_dawn_warden", "eternal_light_sovereign"
-  ],
-
-  worlds_end_expanse: [
-    "titan_blooded_ruinshaper", "cosmic_crowned_terminarch",
-    "god", "void_god"
-  ],
-
-  astral_nexus: [
-    "plink_of_the_seventh_dimension", "sthreevens_chaotic_doppelcat",
-    "umbral_skitterfiend", "hollowbone_stalker",
-    "abyss_gnawed_shadehound", "nullborn_ravager",
-    "runespun_aetherling", "eclipse_marked_assassin",
-    "spellforged_myrmidon", "astral_thread_weaver",
-    "warped_soulrender", "mana_starved_riftspawn",
-    "grimhollow_reaperling", "arc_bound_sentinel",
-    "hate_crowned_dreadmonger", "arcane_pulse_serpent",
-    "root_bound_sentinel", "null_eater_sovereignling",
-    "spell_riven_archonling", "mana_crested_riftlord",
-    "astral_veined_oracle", "chrono_shattered_warden",
-    "null_crowned_riftbeast", "abyss_forged_harrower",
-    "eldritch_maw_tyrant", "void_sundered_reaver",
-    "nullborn_rift_prophet", "eternal_rift_warden",
-    "chaos_spiral_sovereign", "null_eclipse_archfiend"
-  ]
-};
+  return map;
+})();
