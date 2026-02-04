7a95e868e3a315ea8c2df4abe30c9bb1b3f28570
76Natsu76 <80848505+76Natsu76@users.noreply.github.com>
2026-02-03 20:03:40 -0500

    Create enemy-group-sizes.js

---
+++ enemy-group-sizes.js
@@ -0,0 +1,21 @@
+// enemy_group_sizes.js
+export const ENEMY_GROUP_SIZES = {
+  wolf: { min: 2, max: 4 },
+  dire_wolf: { min: 2, max: 3 },
+  howling_warg: { min: 2, max: 3 },
+
+  goblin: { min: 2, max: 5 },
+  hobgoblin: { min: 1, max: 3 },
+  bandit: { min: 1, max: 3 },
+
+  hornet_swarm: { min: 3, max: 6 },
+  tiny_slime: { min: 2, max: 5 },
+  sticky_slime: { min: 2, max: 4 },
+
+  skeleton: { min: 1, max: 3 },
+  skeleton_archer: { min: 1, max: 2 },
+  zombie: { min: 1, max: 3 },
+  ghoul: { min: 1, max: 2 },
+
+  default: { min: 1, max: 1 }
+};
