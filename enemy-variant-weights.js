c4cf74a8721816e743fa50aa257dd51c670bf696
76Natsu76 <80848505+76Natsu76@users.noreply.github.com>
2026-02-04 21:21:37 -0500

    Create enemy-variant-weights.js

---
+++ enemy-variant-weights.js
@@ -0,0 +1,25 @@
+export const ENEMY_VARIANT_WEIGHTS = {
+  wolf: {
+    wolf: 70,
+    dire_wolf: 20,
+    alpha_wolf: 10
+  },
+
+  goblin: {
+    goblin: 70,
+    goblin_archer: 15,
+    goblin_brute: 10,
+    goblin_shaman: 5
+  },
+
+  skeleton: {
+    skeleton: 70,
+    skeleton_archer: 20,
+    skeleton_mage: 8,
+    skeleton_captain: 2
+  },
+
+  default: {
+    base: 100
+  }
+};
