cb65198638b8cdd1952ce59d5fe7f30c428eb82f
76Natsu76 <80848505+76Natsu76@users.noreply.github.com>
2026-02-06 03:16:16 -0500

    Create profession-environment-synergies.js

---
+++ profession-environment-synergies.js
@@ -0,0 +1,32 @@
+export const PROFESSION_ENVIRONMENT_SYNERGIES = {
+  ranger: {
+    tags: ["forest", "deepwild", "overgrown", "primeval"],
+    atkMult: 1.10,
+    accuracyMult: 1.10,
+    dodgeMult: 1.05
+  },
+
+  mage: {
+    tags: ["arcane-surge", "astral", "starlit", "arcane"],
+    atkMult: 1.15,
+    critMult: 1.10
+  },
+
+  paladin: {
+    tags: ["radiant", "celestial", "holy", "starlit"],
+    defMult: 1.10,
+    hpMult: 1.10
+  },
+
+  rogue: {
+    tags: ["shadowed", "void-touched", "labyrinthine"],
+    spdMult: 1.10,
+    critMult: 1.10
+  },
+
+  berserker: {
+    tags: ["infernal", "magma-scorched", "sunscorched"],
+    atkMult: 1.15,
+    hpMult: 1.05
+  }
+};
