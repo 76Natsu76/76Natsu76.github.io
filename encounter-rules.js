8622f8744436029dbe683e8c9c991d2fa9cb76f0
76Natsu76 <80848505+76Natsu76@users.noreply.github.com>
2026-02-06 20:56:00 -0500

    Update encounter-rules.js

--- encounter-rules.js
+++ encounter-rules.js
@@ -1,244 +1,40 @@
 // encounter-rules.js
 
-export const ENCOUNTER_RULES = {
-  region: {
-    "forest-edge": {
-      "common":   { "tiers": [1, 2] },
-      "uncommon": { "tiers": [2] },
-      "rare":     { "tiers": [2, 3] },
-      "boss":     { "tiers": [3] }
-    },
-    "deep-forest": {
-      "common":   { "tiers": [2] },
-      "uncommon": { "tiers": [2, 3] },
-      "rare":     { "tiers": [3] },
-      "boss":     { "tiers": [4] }
-    },
-    "plains-field": {
-      "common":   { "tiers": [1] },
-      "uncommon": { "tiers": [1, 2] },
-      "rare":     { "tiers": [2] },
-      "boss":     { "tiers": [3] }
-    },
-    "swamp-marsh": {
-      "common":   { "tiers": [2] },
-      "uncommon": { "tiers": [2, 3] },
-      "rare":     { "tiers": [3] },
-      "boss":     { "tiers": [4] }
-    },
-    "desert-dunes": {
-      "common":   { "tiers": [2] },
-      "uncommon": { "tiers": [2, 3] },
-      "rare":     { "tiers": [3] },
-      "boss":     { "tiers": [4] }
-    },
-    "verdant-woods": {
-      "common":   { "tiers": [1, 2] },
-      "uncommon": { "tiers": [2] },
-      "rare":     { "tiers": [3] },
-      "boss":     { "tiers": [4] }
-    },
-  
-    "frostlands": {
-      "common":   { "tiers": [2] },
-      "uncommon": { "tiers": [2, 3] },
-      "rare":     { "tiers": [3, 4] },
-      "boss":     { "tiers": [5] }
-    },
-  
-    "volcanic-wastes": {
-      "common":   { "tiers": [3] },
-      "uncommon": { "tiers": [3, 4] },
-      "rare":     { "tiers": [4, 5] },
-      "boss":     { "tiers": [6] }
-    },
-  
-    "primordial-grove": {
-      "common":   { "tiers": [3] },
-      "uncommon": { "tiers": [3, 4] },
-      "rare":     { "tiers": [4, 5] },
-      "boss":     { "tiers": [6] }
-    },
-  
-    "celestial-expanse": {
-      "common":   { "tiers": [4] },
-      "uncommon": { "tiers": [4, 5] },
-      "rare":     { "tiers": [5, 6] },
-      "boss":     { "tiers": [7] }
-    },
-  
-    "eternal-citadel": {
-      "common":   { "tiers": [4] },
-      "uncommon": { "tiers": [4, 5] },
-      "rare":     { "tiers": [5, 6] },
-      "boss":     { "tiers": [7] }
-    },
-  
-    "worldbreaker-horizon": {
-      "common":   { "tiers": [5] },
-      "uncommon": { "tiers": [5, 6] },
-      "rare":     { "tiers": [6, 7] },
-      "boss":     { "tiers": [7] }
-    },
-  
-    "astral-nexus": {
-      "common":   { "tiers": [5] },
-      "uncommon": { "tiers": [5, 6] },
-      "rare":     { "tiers": [6, 7] },
-      "boss":     { "tiers": [7] }
-    },
-  
-    "void-realm": {
-      "common":   { "tiers": [4] },
-      "uncommon": { "tiers": [4, 5] },
-      "rare":     { "tiers": [5, 6] },
-      "boss":     { "tiers": [6, 7] }
-    },
-  
-    "abyssal-deep": {
-      "common":   { "tiers": [5] },
-      "uncommon": { "tiers": [5, 6] },
-      "rare":     { "tiers": [6, 7] },
-      "boss":     { "tiers": [7] }
-    },
-  
-    "forest": {
-      "common":   { "tiers": [1] },
-      "uncommon": { "tiers": [1, 2] },
-      "rare":     { "tiers": [2] },
-      "boss":     { "tiers": [3] }
-    },
-  
-    "plains": {
-      "common":   { "tiers": [1] },
-      "uncommon": { "tiers": [1, 2] },
-      "rare":     { "tiers": [2] },
-      "boss":     { "tiers": [3] }
-    },
-  
-    "swamp": {
-      "common":   { "tiers": [2] },
-      "uncommon": { "tiers": [2, 3] },
-      "rare":     { "tiers": [3] },
-      "boss":     { "tiers": [4] }
-    },
-  
-    "desert": {
-      "common":   { "tiers": [2] },
-      "uncommon": { "tiers": [2, 3] },
-      "rare":     { "tiers": [3] },
-      "boss":     { "tiers": [4] }
-    },
-  
-    "tundra": {
-      "common":   { "tiers": [2] },
-      "uncommon": { "tiers": [2, 3] },
-      "rare":     { "tiers": [3] },
-      "boss":     { "tiers": [4] }
-    },
-  
-    "mountains": {
-      "common":   { "tiers": [2] },
-      "uncommon": { "tiers": [2, 3] },
-      "rare":     { "tiers": [3, 4] },
-      "boss":     { "tiers": [5] }
-    },
-  
-    "void": {
-      "common":   { "tiers": [4] },
-      "uncommon": { "tiers": [4, 5] },
-      "rare":     { "tiers": [5, 6] },
-      "boss":     { "tiers": [6, 7] }
-    },
-  
-    "celestial": {
-      "common":   { "tiers": [4] },
-      "uncommon": { "tiers": [4, 5] },
-      "rare":     { "tiers": [5, 6] },
-      "boss":     { "tiers": [6, 7] }
-    },
-  
-    "arcane-rift": {
-      "common":   { "tiers": [4] },
-      "uncommon": { "tiers": [4, 5] },
-      "rare":     { "tiers": [5] },
-      "boss":     { "tiers": [6] }
-    },
-  
-    "elderwood-heart": {
-      "common":   { "tiers": [3] },
-      "uncommon": { "tiers": [3, 4] },
-      "rare":     { "tiers": [4, 5] },
-      "boss":     { "tiers": [6] }
-    },
-  
-    "primeval-overgrowth": {
-      "common":   { "tiers": [3] },
-      "uncommon": { "tiers": [3, 4] },
-      "rare":     { "tiers": [4, 5] },
-      "boss":     { "tiers": [6] }
-    },
-  
-    "celestial-horizon": {
-      "common":   { "tiers": [5] },
-      "uncommon": { "tiers": [5, 6] },
-      "rare":     { "tiers": [6, 7] },
-      "boss":     { "tiers": [7] }
-    },
-  
-    "worlds-end-expanse": {
-      "common":   { "tiers": [5] },
-      "uncommon": { "tiers": [5, 6] },
-      "rare":     { "tiers": [6, 7] },
-      "boss":     { "tiers": [7] }
-    }
-  },
+import { REGION_ENCOUNTER_TABLES } from "./region-encounter-tables.js";
 
-  biome: {
-    forest: {
-      families: {
-        beast:  3,
-        fae:    1,
-        plant:  2
-      }
-    },
-    plains: {
-      families: {
-        beast:   2,
-        human:   2,
-        bandit:  2
-      }
-    },
-    swamp: {
-      families: {
-        undead:  2,
-        beast:   2,
-        hag:     1
-      }
-    }
-  },
+export const ENCOUNTER_RULES = buildEncounterRules(REGION_ENCOUNTER_TABLES);
 
-  weather: {
-    storm: {
-      rarityMult: { rare: 1.2, boss: 1.1 }
-    },
-    fog: {
-      familyMult: { undead: 1.3 }
-    }
-  },
+function buildEncounterRules(regionEncounterTables) {
+  const region = {};
 
-  crisis: {
-    beastUprising: {
-      familyMult: { beast: 1.5 }
-    },
-    undeadRising: {
-      familyMult: { undead: 1.7 }
+  for (const [regionKey, entry] of Object.entries(regionEncounterTables)) {
+    const weights = {};
+    for (const [rarity, data] of Object.entries(entry)) {
+      // old: { tiers: [1,2] }
+      // new: { tiers: [...], weight: default }
+      weights[rarity] = {
+        tiers: data.tiers || [],
+        weight: defaultRarityWeight(rarity)
+      };
     }
-  },
-
-  event: {
-    harvestFestival: {
-      rarityMult: { common: 1.1, rare: 1.1 }
-    }
+    region[regionKey] = { weights };
   }
-};
+
+  return {
+    region,
+    biome: {},
+    weather: {},
+    crisis: {},
+    event: {}
+  };
+}
+
+function defaultRarityWeight(rarity) {
+  switch (rarity) {
+    case "common":   return 60;
+    case "uncommon": return 25;
+    case "rare":     return 10;
+    case "boss":     return 5;
+    default:         return 10;
+  }
+}
