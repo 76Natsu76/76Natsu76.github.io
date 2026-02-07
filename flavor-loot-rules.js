8fc2e73eb0acde9e426bc7b92948cd1ed36974a9
76Natsu76 <80848505+76Natsu76@users.noreply.github.com>
2026-02-06 21:25:52 -0500

    Create flavor-loot-rules.js

---
+++ flavor-loot-rules.js
@@ -0,0 +1,9 @@
+// flavor-loot-rules.js
+export const FLAVOR_LOOT_RULES = {
+  "storm-kissed": ["Lightning‑Charged Shard"],
+  "void-touched": ["Void‑Infused Dust"],
+  "frostbitten": ["Frost‑Dusted Herb"],
+  "sunscorched": ["Sun‑Cracked Resin"],
+  "deepwild": ["Ancient Bark Fragment"],
+  "astral": ["Starlight Dust"]
+};
