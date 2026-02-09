6bda1eed3e4f58b209f3a707e6e0bef67f30c4e5
76Natsu76 <80848505+76Natsu76@users.noreply.github.com>
2026-02-09 02:31:32 -0500

    Create economy-resources.js

---
+++ economy-resources.js
@@ -0,0 +1,10 @@
+// economy-resources.js
+
+export const RESOURCES = {
+  wood: { name: "Wood", baseValue: 2 },
+  ore: { name: "Ore", baseValue: 5 },
+  herbs: { name: "Herbs", baseValue: 3 },
+  leather: { name: "Leather", baseValue: 4 },
+  cloth: { name: "Cloth", baseValue: 3 },
+  essence: { name: "Essence", baseValue: 10 }
+};
