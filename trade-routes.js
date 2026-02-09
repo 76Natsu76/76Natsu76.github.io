97cba91b78f88283271d7b6c71b38b8171e1ad42
76Natsu76 <80848505+76Natsu76@users.noreply.github.com>
2026-02-09 02:40:51 -0500

    Create trade-routes.js

---
+++ trade-routes.js
@@ -0,0 +1,6 @@
+// trade-routes.js
+
+export const TRADE_ROUTES = [
+  { from: "greenhaven", to: "emberfall", resource: "herbs", rate: 1 },
+  { from: "emberfall", to: "frostwatch", resource: "ore", rate: 1 }
+];
