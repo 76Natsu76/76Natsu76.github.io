d4f73487a4aa58892388841d248bcd616918c998
76Natsu76 <80848505+76Natsu76@users.noreply.github.com>
2026-02-15 23:48:51 -0500

    Create identity-index.js

---
+++ identity-index.js
@@ -0,0 +1,4 @@
+// identity-index.js
+export { REGION_IDENTITY } from "./region-identity.js";
+export { BIOME_IDENTITY } from "./biome-identity.js";
+export { SUBREGION_IDENTITY } from "./subregion-identity.js";
