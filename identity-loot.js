b93f8078651990916e0785a9b33e397cfb36c9fc
76Natsu76 <80848505+76Natsu76@users.noreply.github.com>
2026-02-16 22:13:11 -0500

    Create identity-loot.js

---
+++ identity-loot.js
@@ -0,0 +1,68 @@
+export const PROFESSION_LOOT = {
+  warrior: ["iron_scrap", "weapon_parts"],
+  rogue: ["poison_gland", "shadow_residue"],
+  ranger: ["beast_hide", "arrow_bundle"],
+  berserker: ["rage_fragment"],
+  monk: ["chi_fragment"],
+  pyromancer: ["fire_essence"],
+  cryomancer: ["frost_shard"],
+  electromancer: ["storm_core"],
+  geomancer: ["earth_chunk"],
+  necromancer: ["necrotic_dust"],
+  cleric: ["holy_water"],
+  paladin: ["radiant_shard"],
+  druid: ["nature_bloom"],
+  shaman: ["spirit_wisp"],
+  spellblade: ["arcane_dust"],
+  // … add more as desired
+};
+
+export const ELEMENT_LOOT = {
+  fire: ["fire_essence"],
+  ice: ["frost_shard"],
+  electric: ["storm_core"],
+  earth: ["earth_chunk"],
+  wind: ["air_sigil"],
+  water: ["water_globule"],
+  nature: ["nature_bloom"],
+  poison: ["toxin_sac"],
+  holy: ["radiant_shard"],
+  dark: ["shadow_residue"],
+  void: ["void_essence"],
+  arcane: ["arcane_dust"],
+  chaos: ["chaos_fragment"],
+  cosmic: ["cosmic_thread"],
+  metal: ["metal_scrap"],
+  spirit: ["spirit_wisp"]
+};
+
+export const SUBRACE_LOOT = {
+  skeleton: ["bone_fragment"],
+  ghoul: ["rotting_flesh"],
+  wraith: ["ectoplasm"],
+  lich: ["phylactery_shard"],
+  beast: ["beast_hide"],
+  "dire-beast": ["thick_hide"],
+  "spirit-beast": ["spirit_wisp"],
+  golem: ["stone_core"],
+  automaton: ["gear_fragment"],
+  "arcane-construct": ["arcane_core"],
+  voidborn: ["void_essence"],
+  slime: ["slime_gel"]
+};
+
+export const VARIANT_LOOT = {
+  elite: ["rare_shard"],
+  champion: ["greater_rare_shard"],
+  mythic: ["mythic_core"],
+  frostbitten: ["frost_shard"],
+  "flame-touched": ["fire_essence"],
+  stormcharged: ["storm_core"],
+  corrupted: ["toxin_sac"],
+  "void-touched": ["void_essence"],
+  "chaos-mutated": ["chaos_fragment"],
+  enraged: ["rage_fragment"],
+  berserk: ["rage_fragment"],
+  "arcane-infused": ["arcane_dust"],
+  "cosmic-aligned": ["cosmic_thread"]
+};
