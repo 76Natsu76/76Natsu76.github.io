ee760831e1451da2e398b91804613521fbf76acf
76Natsu76 <80848505+76Natsu76@users.noreply.github.com>
2026-02-16 22:23:47 -0500

    Create identity-bonuses.js

---
+++ identity-bonuses.js
@@ -0,0 +1,14 @@
+export const IDENTITY_BONUSES = {
+  fire: {
+    threshold: 10,
+    bonus: { fireResist: 0.02 }
+  },
+  void: {
+    threshold: 10,
+    bonus: { corruptionResist: 0.03 }
+  },
+  beast: {
+    threshold: 20,
+    bonus: { beastDamage: 0.05 }
+  }
+};
