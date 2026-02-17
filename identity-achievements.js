d92ce90bf83920f1a32d2ae51c6e6d229c835e58
76Natsu76 <80848505+76Natsu76@users.noreply.github.com>
2026-02-16 22:22:00 -0500

    Create identity-achievements.js

---
+++ identity-achievements.js
@@ -0,0 +1,25 @@
+export const IDENTITY_ACHIEVEMENTS = {
+  "slayer_of_flame": {
+    name: "Slayer of Flame",
+    condition: (enemy) => enemy.element === "fire",
+    progress: 0,
+    goal: 10,
+    reward: { gold: 100, item: "fire_essence" }
+  },
+
+  "void_hunter": {
+    name: "Void Hunter",
+    condition: (enemy) => enemy.element === "void",
+    progress: 0,
+    goal: 5,
+    reward: { item: "void_essence", quantity: 3 }
+  },
+
+  "beast_master": {
+    name: "Beast Master",
+    condition: (enemy) => enemy.subrace === "beast",
+    progress: 0,
+    goal: 20,
+    reward: { title: "Beast Master" }
+  }
+};
